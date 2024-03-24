011cc2d0a04b46d8404d6e2b6b36864c3e112170
David Fernández <dfernandez.martin@hotmail.com>
2024-03-24 16:38:17 +0100

    proyecto para producción clouding.io

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+const app = express();
+const PORT = 3049;
+
+
+const url = 'https://utedevas.es/Mantenimiento/Mantenimiento.php?p=';
+
+
+//** Configurar los cors
+app.use(cors());
+
+app.get('', (req, res) => {
+    res.send('Backend avisos utedevas...');
+})
+
+const loginRoutes = require('./rutas/login');
+app.use('/login', loginRoutes);
+
+
+// Manejar rutas desconocidas
+app.use((req, res) => {
+    res.status(404).send('Ruta no encontrada');
+});
+
+// Escuchar en el puerto especificado
+app.listen(PORT, () => {
+    console.log(`El servidor está escuchando en el puerto ${PORT}`.green);
+});
