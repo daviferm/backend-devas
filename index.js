30111ddde44b61aa7d59db9650a1823f314ea1af
David Fernández <dfernandez.martin@hotmail.com>
2025-03-29 08:45:22 +0100

    modificar index.js

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const PORT = 3049;
 //** Configurar los cors
 app.use(cors());
 
-const options = {
-    key: fs.readFileSync(path.join(__dirname, 'ssl/private.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'ssl/certificado.cer'))
-  };
-
 app.get('', (req, res) => {
     res.send('------dvs.backend.es  🥶--------');
 })
@@ -27,6 +22,6 @@ app.use((req, res) => {
 });
 
 // Escuchar en el puerto especificado
-https.createServer(options, app).listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`El servidor está escuchando en el puerto ${PORT}`.green);
 });
